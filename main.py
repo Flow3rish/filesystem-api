0110dd5b27adae82cd87dbcb63e1f9a20424ab71
Vit Chrubasik <flow3rish@protonmail.com>
2020-11-01 23:35:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from lib.filesys_functions.info_functions import list_all_with_info, list_detail_with_info
+from lib.api.path_functions import apipath_to_path, path_to_apipath
+from lib.constants import API_ROOT_DIR
+from fastapi import FastAPI
+
+# TODO: implement endpoints
+
+app = FastAPI()
